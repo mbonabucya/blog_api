683f97df14cf40c9422cb1ddbb49dbbff5a05f81
mbonabucya <mbonajames12@gmail.com>
2023-03-08 11:41:49 -0500

    push the blog REST API

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
